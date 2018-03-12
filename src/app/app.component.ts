0fd2574fcf48063082556f7b0babac2a4a134ed9
Jan <jfriese82@gmail.com>
2018-03-12 18:27:59 +0100

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+declare const require: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  title = 'Api Overview';
+  apiData = require('../api.json');
+
+  justATest() {
+    console.log(this.apiData);
+  }
 }
+
